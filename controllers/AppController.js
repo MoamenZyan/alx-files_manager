1bfeb2c187c62adcf2d7951275e2d58c52398061
MoamenZyan <moamenzyan20@gmail.com>
2024-09-04 09:59:42 +0300

    fix

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,24 +2,22 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AppController {
+
   static getStatus(req, res) {
-    try {
-      const redis = redisClient.isAlive();
-      const db = dbClient.isAlive();
-      res.status(200).send({ redis, db });
-    } catch (error) {
-      console.log(error);
-    }
+    const status = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    res.status(200).send(status);
   }
 
   static async getStats(req, res) {
-    try {
-      const users = await dbClient.nbUsers();
-      const files = await dbClient.nbFiles();
-      res.status(200).send({ users, files });
-    } catch (error) {
-      console.log(error);
-    }
+    const stats = {
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
+    };
+  
+    res.status(200).send(stats);
   }
 }
 
